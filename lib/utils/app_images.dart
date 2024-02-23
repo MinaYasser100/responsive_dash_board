59bd121965606f1450f329e41ab1a1bc6902e821
MinaYasser100 <minayasser43@gmail.com>
2024-02-23 03:46:18 +0200

    add all assets (image and fonts)

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,63 @@
+class Assets {
+  Assets._();
+
+  /// Assets for imagesCardReceive
+  /// assets/images/card-receive.svg
+  static const String imagesCardReceive = "assets/images/card-receive.svg";
+
+  /// Assets for imagesCardSend
+  /// assets/images/card-send.svg
+  static const String imagesCardSend = "assets/images/card-send.svg";
+
+  /// Assets for imagesCategory2
+  /// assets/images/category-2.svg
+  static const String imagesCategory2 = "assets/images/category-2.svg";
+
+  /// Assets for imagesChart2
+  /// assets/images/chart-2.svg
+  static const String imagesChart2 = "assets/images/chart-2.svg";
+
+  /// Assets for imagesConvertCard
+  /// assets/images/convert-card.svg
+  static const String imagesConvertCard = "assets/images/convert-card.svg";
+
+  /// Assets for imagesFrame
+  /// assets/images/Frame.svg
+  static const String imagesFrame = "assets/images/Frame.svg";
+
+  /// Assets for imagesFrameOne
+  /// assets/images/Frame_one.svg
+  static const String imagesFrameOne = "assets/images/Frame_one.svg";
+
+  /// Assets for imagesFrameThree
+  /// assets/images/Frame_three.svg
+  static const String imagesFrameThree = "assets/images/Frame_three.svg";
+
+  /// Assets for imagesFrameTwo
+  /// assets/images/Frame_two.svg
+  static const String imagesFrameTwo = "assets/images/Frame_two.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesMask group
+  /// assets/images/Mask group.svg
+  static const String imagesMaskgroup = "assets/images/Mask group.svg";
+
+  /// Assets for imagesMoneys
+  /// assets/images/moneys.svg
+  static const String imagesMoneys = "assets/images/moneys.svg";
+
+  /// Assets for imagesSetting2
+  /// assets/images/setting-2.svg
+  static const String imagesSetting2 = "assets/images/setting-2.svg";
+
+  /// Assets for imagesStatistic
+  /// assets/images/statistic.svg
+  static const String imagesStatistic = "assets/images/statistic.svg";
+
+  /// Assets for imagesWallet2
+  /// assets/images/wallet-2.svg
+  static const String imagesWallet2 = "assets/images/wallet-2.svg";
+}
